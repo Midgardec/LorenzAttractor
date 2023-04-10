e4f0598f597093f75d5f4c01bd6e71cc96d5fffc
Midgardec <valievgorg@gmail.com>
2023-04-10 04:26:13 +0300

    added some tweaks with displaying points + some controls to rotation and drawing (stopping/continuing)

--- main.cpp
+++ main.cpp
@@ -9,6 +9,8 @@ float rho = 28.0;
 
 const int numPointsPerCurve = 100000000;
 const float dt = 0.0001;
+bool stopDrawing = false;
+bool stopRotating = false;
 
 struct Point {
     float x, y, z;
@@ -23,11 +25,14 @@ struct Curve {
 
 std::vector<Curve> curves;
 
-void addCurve() {
-    float x = ((float) rand() / (float) RAND_MAX - 0.5) * 20.0;
+void addCurve(float offset) {
+    /*float x = ((float) rand() / (float) RAND_MAX - 0.5) * 20.0;
     float y = ((float) rand() / (float) RAND_MAX - 0.5) * 20.0;
-    float z = ((float) rand() / (float) RAND_MAX) * 50.0;
+    float z = ((float) rand() / (float) RAND_MAX) * 50.0;*/
 
+    float x = 10 + offset;
+    float y = 0;
+    float z = 0;
     Curve curve;
     curve.color[0] = (float) rand() / (float) RAND_MAX;
     curve.color[1] = (float) rand() / (float) RAND_MAX;
@@ -53,95 +58,122 @@ void drawCurves() {
         glBegin(GL_LINE_STRIP);
         glColor3f(curve.color[0], curve.color[1], curve.color[2]);
 
-        for (const Point &point: curve.points) {
+        int am = 100;
+        for (int j = std::max(0, static_cast<int>(curve.points.size()) - am); j < curve.points.size(); j++) {
+            const Point &point = curve.points[j];
             glVertex3f(point.x, point.y, point.z);
         }
+
+        /*for (const Point &point: curve.points) {
+            glVertex3f(point.x, point.y, point.z);
+        }*/
 
         glEnd();
     }
 }
-GLfloat angle=0;
+
+GLfloat angle = 0;
+
 void display() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity();
 
-    float eyeX = 80 * sin(0.5);
-    float eyeY = 50;
-    float eyeZ = 80 * cos(0.5);
+    float eyeX = 120.f * sin(0.5);
+    float eyeY = 50.f;
+    float eyeZ = 120.f * cos(0.5);
     float centerX = 0;
-    float centerY = 0;
+    float centerY = 50;
     float centerZ = 0;
 
     gluLookAt(eyeX, eyeY, eyeZ, centerX, centerY, centerZ, 0, 1, 0);
-    angle+=1.f;
+    if (!stopRotating) {
+        angle += 0.1f;
+
+    }
     glRotatef(angle, 0.0f, 1.0f, 0.0f);
+    glRotatef(90.f, -1.0f, 0.0f, 0.0f);
+
     drawCurves();
 
     glutSwapBuffers();
 }
+
+
 void keyboard(unsigned char key, int x, int y) {
     switch (key) {
         case 'q':
+            stopDrawing = !stopDrawing;
+            break;
+        case 'Q':
+            stopDrawing = !stopDrawing;
+            break;
+        case 'r':
+            stopRotating = !stopRotating;
+            break;
+        case 'R':
+            stopRotating = !stopRotating;
+            break;
         case 27: // ESC key
             exit(0);
             break;
     }
 }
+
 void animate(int value) {
-    for (Curve &curve: curves) {
-        Point &lastPoint = curve.points.back();
-        float x = lastPoint.x;
-        float y = lastPoint.y;
-        float z = lastPoint.z;
+    if (!stopDrawing) {
+        for (Curve &curve: curves) {
+            Point &lastPoint = curve.points.back();
+            float x = lastPoint.x;
+            float y = lastPoint.y;
+            float z = lastPoint.z;
 
-        for (int i = 0; i < 100; i++) {
-            float dx = sigma * (y - x);
-            float dy = x * (rho - z) - y;
-            float dz = x * y - beta * z;
+            for (int i = 0; i < 100; i++) {
+                float dx = sigma * (y - x);
+                float dy = x * (rho - z) - y;
+                float dz = x * y - beta * z;
 
-            x += dx * dt;
-            y += dy * dt;
-            z += dz * dt;
+                x += dx * dt;
+                y += dy * dt;
+                z += dz * dt;
+
+            }
+            curve.points.emplace_back(x, y, z);
+
         }
 
-        curve.points.emplace_back(x, y, z);
-    }
 
+    }
     glutPostRedisplay();
-    glutTimerFunc(50,animate, value); // вызываем функцию animate снова через 25 миллисекунд
+    glutTimerFunc(10/10, animate, value); // вызываем функцию animate снова через 1 миллисекунд
 }
 
 void reshape(int width, int height) {
     glViewport(0, 0, width, height);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    gluPerspective(60, (float)width / (float)height, 1, 1000);
+    gluPerspective(70, (float) width / (float) height, 1, 1000);
     glMatrixMode(GL_MODELVIEW);
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
     glutInitWindowSize(1920, 1080);
-    glutCreateWindow("Lorenz Attractor");glEnable(GL_DEPTH_TEST);
+    glutCreateWindow("Lorenz Attractor");
+    glEnable(GL_DEPTH_TEST);
 
     glutDisplayFunc(display);
     glutReshapeFunc(reshape);
     glutKeyboardFunc(keyboard);
     glutTimerFunc(10, animate, 0);
 
-    addCurve();
-    addCurve();
-    addCurve();
-    addCurve();
-    addCurve();
-    addCurve();
-    addCurve();
-    addCurve();
-    addCurve();
-    addCurve();
-    addCurve();
-    addCurve();
+    addCurve(0.1);
+    addCurve(0.101);
+    addCurve(0.2);
+    addCurve(0.201);
+    addCurve(0.3);
+    addCurve(0.301);
+
 
     glutMainLoop();
     return 0;
